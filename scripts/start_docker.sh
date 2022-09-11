47eeda0771ec3c6288ba47db3881b74535ac1e13
Gaurav Gupta <gaurav@blackcoffeerobotics.com>
2022-09-11 21:07:00 +0530

    introduced ddsconfig as param

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-docker_name="slim_bridge"
+if [[ -z "${ROBOT_NAME}" ]]; then
+  docker_name=slim_bridge
+else
+  docker_name=${ROBOT_NAME}_slim_bridge
+fi
 
 docker run --detach --privileged --net=host \
 --name $docker_name \
+--env RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+--env ROBOT_NAME=$ROBOT_NAME \
+--env CYCLONEDDS_URI=file:///ddsconfig.xml \
+-v $PWD/ddsconfig.xml:/ddsconfig.xml \
 -v $PWD/initial_topics.yaml:/root/bridge_ws/install/slim_bridge/share/slim_bridge/config/initial_topics.yaml \
  slim_bridge:latest ./cmd_script.sh
--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker_name="slim_bridge"
+if [[ -z "${ROBOT_NAME}" ]]; then
+  docker_name=slim_bridge
+else
+  docker_name=${ROBOT_NAME}_slim_bridge
+fi
 docker stop $docker_name && docker rm $docker_name
